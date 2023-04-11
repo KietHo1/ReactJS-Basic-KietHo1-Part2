75c63d0769bcaa30f6fc18f03895d1e26cee8bb0
KietHo1 <kiethostartup1999@gmail.com>
2023-04-11 14:14:26 +0700

    video 4 React season 2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,21 @@ import logo from "./logo.svg";
 import "./App.css";
 
 const App = () => {
-  // template + logic
-  // JSX
-  //babel
+  let name = "Eric"; //string
+  let number = 2021;
+  let obj = { name: "ERic", channel: "Hoi Dan IT" };
+  let link =
+    "https://www.youtube.com/watch?v=frVHVZDpsp4&list=PLncHg6Kn2JT4xzJyhXfmJ53dzwVbq-S_E";
   return (
     <div className="App">
+      {console.log("check obj: ", obj)}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world with React and Hoi Dan IT!</h1>
+        <h1>Hello world with React and {name}!</h1>
+        <p>{JSON.stringify(obj)}</p>
+        <a href={link} target="_blank">
+          Visit my channel
+        </a>
       </header>
     </div>
   );
